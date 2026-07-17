554efab86b660bcdf597a3e162a65aa5a503cb8e
Giovanna Duarte <giovannaduartedasilva1213@gmail.com>
2026-07-17 14:47:04 -0300

    Padroniza os campos de texto na etapa de transformação.

--- scripts/Transform.py
+++ scripts/Transform.py
@@ -17,8 +17,27 @@ df.drop_duplicates(
 inplace=True
  )
 
-print('\nQuantidade de linhas após remover duplacados:') 
+print('\nQuantidade de linhas após remover duplicados:') 
 print(len(df)) 
 
 print('\nDados após a limpeza:') 
 print(df) 
+
+print ('\nModelos antes da padronização') 
+print(df['model'].unique())  
+
+print ('\npadronizando modelos')
+df ['model'] = df ['model'].str.title() 
+print('modelos padronizados') 
+print (df['model'].unique())
+
+print ('padronização coluna de textos') 
+coluna_texto = ["dealership",
+    "brand",
+    "model",
+    "color",
+    "city"]
+for coluna in coluna_texto:
+    df[coluna] = df[coluna].str.title() 
+print ('colunas padronizadas') 
+print (df) 
